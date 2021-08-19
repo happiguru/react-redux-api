3de91a53eeb82c2ccca3df12657606eadf50dce6
happiguru <hguruman@gmail.com>
2021-08-19 18:16:48 +0400

    Add store to Provider and set the router properly.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import Home from 'containers/Home';
+import FoodsDetail from 'components/FoodsDetail';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <div className="main-container">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:id" component={FoodsDetail} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
