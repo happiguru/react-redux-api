57ebc09f6371abbdf61a5a436d9ef6a6c370f3e3
happiguru <hguruman@gmail.com>
2021-08-26 01:34:01 +0400

    Align footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => (
-  <footer className="header text-decoration-none footer ">
+  <footer className="header text-decoration-none footer text-center">
     <Link className="link footer-link" to="/">Thank You For Using Foodistics</Link>
     <p className="span">Happi Guru</p>
   </footer>
