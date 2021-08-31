09e06b3d699bdb96bc03b0cd8ceebbdbd2915eff
happiguru <hguruman@gmail.com>
2021-08-31 20:07:02 +0400

    Add filterFoods component.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import FoodList from './FoodList';
-import { addFoods } from '../actions/Foods';
+import { useDispatch, useSelector } from 'react-redux';
+import FoodsList from '../components/FoodsList';
+import FilterFoods from '../components/FilterFoods';
+import { loadFoods, setFilter } from '../actions/loadFoodAction';
 
 const FoodItems = () => {
   const dispatch = useDispatch();
+  const filter = useSelector((state) => state.filter);
+
+  const [foods, setFoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const handleFilterChange = (filterData) => {
+    dispatch(setFilter(filterData));
+  };
+
   const fetchData = async () => {
     try {
-      const { data } = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?f=c');
-      dispatch(addFoods(data.meals));
-      const el = document.querySelector('.error');
-      el.innerHTML = '';
+      const { data } = await axios.get(
+        'https://www.themealdb.com/api/json/v1/1/search.php?f=c',
+      );
+      setFoods(data.meals);
+      dispatch(loadFoods(data.meals));
+      setLoading(false);
     } catch (error) {
       const el = document.querySelector('.error');
       el.innerHTML = 'Sorry, please try again!';
@@ -21,10 +32,14 @@ const FoodItems = () => {
   useEffect(() => {
     fetchData();
   }, []);
-
+  if (loading) {
+    return <div className="loader">loading</div>;
+  }
   return (
     <div className="container home">
-      <FoodList />
+      <FilterFoods changeFilter={handleFilterChange} />
+      <div className="my-3 text-center error" />
+      <FoodsList foodData={foods} filterData={filter.title} />
     </div>
   );
 };
